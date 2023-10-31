d408fd3d6c654300145edbad2878f74b3242b396
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2023-10-31 23:09:15 +0300

    Fix node placement for numbers smaller than first node

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,5 +1,6 @@
 #include "lists.h"
 #include <stdlib.h>
+listint_t *insert_negative(listint_t **head, int number);
 /**
  * insert_node - inserts a node with a value in a sorted singly linked list
  * @head: address of the head of the list
@@ -14,28 +15,32 @@ listint_t *insert_node(listint_t **head, int number)
 
 	if (head == NULL || *head == NULL)
 		return (NULL);
-
-	while (current->next != NULL)
-	{
-		if ((current->next)->n >= number)
-		{
-			temp  = current->next;
-			new_node = malloc(sizeof(listint_t));
-			if (new_node == NULL)
-				return (NULL);
-			new_node->n = number;
-			current->next = new_node;
-			new_node->next = temp;
-			return (new_node);
-
-		}
-		current = current->next;
-	}
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = number;
+	/*if number is less than first node*/
+	if (current == NULL || current->n >= number)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
+	/*if number ranges between 2nd to last node*/
+	while (current->next != NULL)
+	{
+		if ((current->next)->n >= number)
+		{
+			temp = current->next;
+			current->next = new_node;
+			new_node->next = temp;
+			return (new_node);
+		}
+		current = current->next;
+	}
+	/*if number is greater than all nodes*/
 	new_node->next = NULL;
 	current->next = new_node;
 	return (new_node);
 }
+
