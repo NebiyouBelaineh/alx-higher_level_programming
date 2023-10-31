f2638553d4994e88e6766e6af824d9500b8437af
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2023-10-31 16:24:59 +0300

    Add 6-print_comb3 for task 6

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+for d2 in range(0, 9):
+    for d1 in range(0, 10):
+        if d1 > d2:
+            if d2 == 8:
+                 print("{0:d}{1:d}".format(d2, d1))
+                 break
+            print("{0:d}{1:d}".format(d2, d1), end=", ")
