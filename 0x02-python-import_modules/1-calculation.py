fa04e004735369c8789e5796f065281361c89904
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2023-11-02 15:17:03 +0300

    Change import method

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div as add, sub, mul, div
+    from calculator_1 import add as add, sub as sub, mul as mul, div as div
     a = 10
     b = 5
     print("{0:d} + {1:d} = {2:d}".format(a, b, add(a, b)))
