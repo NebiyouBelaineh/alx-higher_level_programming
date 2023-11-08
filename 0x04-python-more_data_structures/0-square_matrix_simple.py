5fda181c80be01ddc03d67adf0f9f2a5f79584f6
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2023-11-08 17:22:12 +0300

    Fix pycodestyle error

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -4,5 +4,7 @@ def square_matrix_simple(matrix=[]):
     new_matrix = [[]]
     new_matrix = list(map(square, matrix))
     return (new_matrix)
+
+
 def square(list=[]):
     return ([i**2 for i in list])
