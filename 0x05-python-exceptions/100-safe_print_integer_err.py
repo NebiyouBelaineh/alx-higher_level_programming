3be5fb92ce31f09be1bfa726bb37014caaca747c
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2023-11-20 18:59:16 +0300

    Fix return value error

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -7,9 +7,12 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
     except ValueError as ve:
         print("Exception: {}".format(ve), file=sys.stderr)
+        return False
     except TypeError as te:
         print("Exception: {}".format(te), file=sys.stderr)
+        return False
     except NameError as ne:
         print("Exception: {}".format(ne), file=sys.stderr)
+        return False
     else:
         return True
