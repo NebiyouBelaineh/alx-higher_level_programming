35dec1b8ec69767001db905992e35033805bbcdc
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2023-11-20 18:56:03 +0300

    Add support for more exceptions

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -7,5 +7,9 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
     except ValueError as ve:
         print("Exception: {}".format(ve), file=sys.stderr)
+    except TypeError as te:
+        print("Exception: {}".format(te), file=sys.stderr)
+    except NameError as ne:
+        print("Exception: {}".format(ne), file=sys.stderr)
     else:
         return True
