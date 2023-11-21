791418dfb397069108d76db497b12a74544e1ba7
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2023-11-22 01:18:21 +0300

    Add solution for task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This module is used to create a class named Square"""
+
+
+class Square:
+    """class Square with """
+
+    def __init__(self, size):
+        """
+        Args:
+            size: is an attribute of class instance
+        """
+        self.__size = size
