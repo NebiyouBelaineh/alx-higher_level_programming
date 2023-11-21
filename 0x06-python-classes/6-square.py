809f267f8aca478d754cfc2be345af734d389e75
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2023-11-22 02:45:24 +0300

    Add conditions to verify tuple type and content

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -76,5 +76,9 @@ class Square:
         Args:
             value (tuple): value to set for __position field
         """
-        if type(value) is not tuple:
+        if type(value) is not tuple or len(value) is not 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if type(value[0]) is not int or type(value[1]) is not int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
