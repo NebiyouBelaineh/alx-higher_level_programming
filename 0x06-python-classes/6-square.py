87d910a5270e2711f55bbcc8ea84ce908b5a4410
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2023-11-22 04:05:02 +0300

    Change back condition for position checking

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -80,9 +80,11 @@ class Square:
             TypeError: if value[0] and value[1] are not integers
             TypeError: if value contains negative numbers
         """
-        if type(value) is not tuple or len(value) != 2 or \
-           type(value[0]) is not int or value[0] < 0 or \
-           type(value[1]) is not int or value[1] < 0:
+        
+        if type(value) is not tuple or len(value) is not 2:
             raise TypeError("position must be a tuple of 2 positive integers")
-        else:
-            self.__position = value
+        if type(value[0]) is not int or type(value[1]) is not int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
