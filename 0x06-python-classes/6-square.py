d583d91ca5121c41e2525276150e64dbeffebf10
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2023-11-22 03:57:40 +0300

    Fix condition for position

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -80,9 +80,9 @@ class Square:
             TypeError: if value[0] and value[1] are not integers
             TypeError: if value contains negative numbers
         """
-        if not isinstance(value, tuple) and len(value) != 2:
+        if not isinstance(value, tuple) or len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
-        if not isinstance(value[0], int) and not isinstance(value[1], int):
+        if not isinstance(value[0], int) or not isinstance(value[1], int):
             raise TypeError("position must be a tuple of 2 positive integers")
         if value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
