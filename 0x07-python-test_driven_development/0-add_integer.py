27da2c774642f536d701410e23eb5cde24c75812
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2023-11-25 15:31:57 +0300

    Add documentaion and fix pycodestyle

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,12 +1,25 @@
 #!/usr/bin/python3
 """Adds two numbers and returns their sum
-   
+
     Examples:
         >>> add_integer(1, 2)
         3"""
+
+
 def add_integer(a, b=98):
-    """
-    Returns the sum of two integers 'a' and 'b'
+    """Returns the sum of two integers 'a' and 'b'
+
+    Args:
+        a (int, float): integer or float paramenter
+        b (int, optional): integer or float parameter. Defaults to 98.
+
+    Raises:
+        TypeError: raised if a or b are not integer or float
+        OverflowError: raised if float('inf') is passed to args a or b
+        ValueError: raised if float('nan') passed to args a or b
+
+    Returns:
+        int : The sum of args a and b
     """
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
@@ -17,6 +30,7 @@ def add_integer(a, b=98):
     if a is float('nan') or b is float('nan'):
         raise ValueError("cannot convert float NaN to integer")
     return int(a) + int(b)
+
 
 if __name__ == '__main__':
     import doctest
