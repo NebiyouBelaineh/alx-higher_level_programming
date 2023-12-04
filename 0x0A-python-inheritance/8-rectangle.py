3aeeb6fbe136a8691cabe2017d949f9750aa48b9
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2023-12-04 20:16:53 +0300

    Add solution for task 8

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""This module defines a class called BaseGeometry"""
+
+
+class BaseGeometry:
+    """class BaseGeometry that contains a method area()"""
+
+    def area(self):
+        """Raises an Exception with the message
+        Raises:
+            Exception: area() is not implemented
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates value if it is an integer or not
+
+        Args:
+            name (string): first parameter
+            value (int): second parameter
+
+        Raises:
+            TypeError: raised if value is not an integer
+            ValueError: raised if value is less than or equal to Zero
+        """
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    def __init__(self, width, height):
+        """Initilizer method for class Rectangle
+
+        Args:
+            width (int): first parameter
+            height (int): second parameter
+        """
+        super().integer_validator("", width)
+        super().integer_validator("", height)
+        self.__width = width
+        self.__height = height
