087231bac729a172691ed4e24a6cf2ff2faee25a
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2023-12-05 22:39:44 +0300

    Remove excess json import

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,11 +2,9 @@
 """This script adds all arguments to a Python list,
 and then saves them to a file: add_item.json"""
 import os
-import json
 import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-
 
 def add_item():
     """Adds the argument to <json_list> everytime it is called"""
@@ -23,8 +21,10 @@ def add_item():
         # convert list to python object using load_from_json_file()
         # append if i not a list, otherwise extend it
         # use save_to_json_file() to save object to file
-        loaded_obj.append(args[i])
-        save_to_json_file(loaded_obj, filename)
-
+        if type(args[i]) is not list:
+            loaded_obj.append(args[i])
+        else:
+            loaded_obj.extend(args[i])
+    save_to_json_file(loaded_obj, filename)
 
 add_item()
