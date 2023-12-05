0ead734c9b28618a4da2616e7742c1fb1f45dd2b
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2023-12-05 21:52:34 +0300

    Add solution for task 7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""This script adds all arguments to a Python list,
+and then saves them to a file: add_item.json"""
+import json
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+# use save_to... for saving to file
+# use load_from ... to load and then append and save again using save_to
+
+
+def add_item(filename, args=[]):
+    """Adds the argument to <json_list> everytime it is called"""
+    if len(args) == 1:
+        save_to_json_file([], filename)
+        return
+    loaded_obj = load_from_json_file(filename)
+    for i in range(1, len(args)):
+        # open file and get list from json file
+        # convert list to python object using load_from_json_file()
+        # append if i not a list, otherwise extend it
+        # use save_to_json_file() to save object to file
+        if type(args[i]) is not list:
+            loaded_obj.append(args[i])
+        else:
+            loaded_obj.extend(args[i])
+    save_to_json_file(loaded_obj, filename)
+
+
+def main():
+    """Main entry point of the program"""
+    import json
+    import sys
+    args = sys.argv
+    filename = 'add_item.json'
+    add_item(filename, args)
+
+
+main()
