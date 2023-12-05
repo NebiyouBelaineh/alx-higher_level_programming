4316363f298bb4c3ce315ca99c965ec14faaacec
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2023-12-05 19:20:51 +0300

    Change import location

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-import json
 """This module defines a function that returns the JSON representation of an
 object(string)"""
 
 
 def to_json_string(my_obj):
+    import json
     """Returns the JSON representation of an object in the form of a
     string"""
     json_rep = json.dumps(my_obj)
