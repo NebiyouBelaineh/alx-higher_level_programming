75cbe5ef956ffb18f0c20b8735c49a55086db5d5
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-02-12 21:21:43 +0300

    Add condition where number is between max and max2

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -10,6 +10,8 @@ if ((argv.length < 4)) {
     if (num > max) {
       max2 = max;
       max = num;
+    } else if (num > max2 && num < max) { // Update max2 if num is greater than max2 but less than max
+      max2 = num;
     }
   }
   if (max2 === -Infinity) {
