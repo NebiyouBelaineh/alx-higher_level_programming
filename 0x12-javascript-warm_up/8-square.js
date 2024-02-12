a0857269974cb6f41969e59ed1add74f930bf8c1
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-02-12 18:57:44 +0300

    Add solution for task 8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const argv = process.argv;
+if (isNaN(parseInt(argv[2]))) {
+  console.log('Missing size');
+} else {
+  const size = parseInt(argv[2]);
+  for (let i = 0; i < size; i++) {
+    let square = '';
+    for (let j = 0; j < size; j++) {
+      square = square + 'x';
+    }
+    console.log(square);
+  }
+}
