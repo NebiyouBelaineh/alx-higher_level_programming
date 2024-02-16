4226463b6d08ffb847e40f2520db04f4049b9d2f
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-02-17 00:21:59 +0300

    Add solution for task 16

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""script that creates the State “California” with the City
+“San Francisco” from the database hbtn_0e_100_usa"""
+
+if __name__ == "__main__":
+    import sys
+    from relationship_state import Base, State
+    from relationship_city import City
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2],
+                                   sys.argv[3]), pool_pre_ping=True)
+
+    # Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    count = 1
+    result = session.query(State).all()
+    for res in result:
+        print(f"{res.id}: {res.name}")
+        for r in res.cities:
+            print(f"    {count}: {r.name}")
+            count += 1
