c63610001f52ed53aeb3227455e987b9a091c813
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-02-15 23:41:03 -0800

    Fix errors for task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,9 +1,14 @@
-"""script that lists all states from the database hbtn_0e_0_usa"""
-import MYSQLdb
+"""
+script that lists all states from the database hbtn_0e_0_usa
+"""
+import sys
+import MySQLdb
 
-conn = MySQLdb.connect(host="localhost", port=3306, user="root", passwd="root", db="hbtn_0e_0_usa", charset="utf8")
+conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                       passwd=sys.argv[2], db=sys.argv[3],
+                       charset="utf8")
 cur = conn.cursor()
-cur.execute("SELECT * FROM states ORDER BY states.id ASC") # HERE I have to know SQL to grab all states in my database
+cur.execute("SELECT * FROM `states`")
 query_rows = cur.fetchall()
 for row in query_rows:
     print(row)
