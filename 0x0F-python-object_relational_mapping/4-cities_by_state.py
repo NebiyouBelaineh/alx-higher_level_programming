b6be9f7275ffce18de15234802a72548495cdb63
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-02-16 14:44:36 +0300

    Add solution for task 4

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""script that lists all cities from the database hbtn_0e_4_usa"""
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
+
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3],
+                           charset="utf8")
+    cur = conn.cursor()
+    query = "SELECT * FROM `cities`"
+    cur.execute(query)
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
