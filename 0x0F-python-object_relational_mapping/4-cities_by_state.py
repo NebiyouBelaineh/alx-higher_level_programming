c7f79c87a71143e317ecac26cf22f9d999eb0b1d
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-02-16 15:13:06 +0300

    Rearrange queries

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -9,7 +9,9 @@ if __name__ == "__main__":
                            passwd=sys.argv[2], db=sys.argv[3],
                            charset="utf8")
     cur = conn.cursor()
-    query = "SELECT * FROM `cities`"
+    query = """SELECT cities.id, cities.name, states.name
+    FROM cities INNER JOIN states ON cities.state_id = states.id
+    ORDER BY cities.id;"""
     cur.execute(query)
     query_rows = cur.fetchall()
     for row in query_rows:
