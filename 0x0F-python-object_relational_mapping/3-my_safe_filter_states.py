ef73048cacefb8d38aa910137a491f77121cebe7
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-02-16 14:15:41 +0300

    Add solution for task 3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""script that is SQL injection protected and takes in an argument and displays
+all values in the states table of hbtn_0e_0_usa where name matches the argument
+"""
+
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
+
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3],
+                           charset="utf8")
+    cur = conn.cursor()
+    name = (sys.argv[4])
+    query = "SELECT * FROM `states` WHERE BINARY `name` = {}".format('%s')
+    cur.execute(query, (sys.argv[4],))
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
