6f17cc883032e3de556082e6afaad6f0e2841b38
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-02-29 22:10:05 +0300

    Remove carriage return using tr

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL and displays all HTTP methods the server will accept.
-curl -sI 0.0.0.0:5000/route_4 | grep -i "Allow" | awk '{for (i=2; i<=NF; i++) printf "%s%s", $i, (i<NF ? " " : "\n")}'
+curl -sI "$1" | grep -i "Allow" | awk '{for (i=2; i<=NF; i++) printf "%s%s", $i, (i<NF ? " " : "\n")}'| tr -d '\r'
