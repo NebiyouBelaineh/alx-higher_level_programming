78ae02e637e401236d2298d4fa5ad0dc4d642e95
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-03-01 01:59:51 +0300

    Change method to recursion

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,7 +3,20 @@
 
 def find_peak(list_of_integers):
     """Finds a peak in a list of integers"""
-    if list_of_integers == []:
+    if list_of_integers == [] or list_of_integers is None:
         return None
-    else:
-    	 return max(list_of_integers)
+    if len(list_of_integers) == 1:
+        return list_of_integers[0]  # return only element inside list
+    if len (list_of_integers) == 2:
+        return max(list_of_integers)  # compare two digits and return max
+    # Three elements and above lists
+    max_index = len(list_of_integers)
+    mid_index = int((max_index) / 2)
+    if list_of_integers[mid_index] >= list_of_integers[mid_index - 1]\
+        and list_of_integers[mid_index] >= list_of_integers[mid_index + 1]:
+            return list_of_integers[mid_index]
+    if mid_index > 0 and list_of_integers[mid_index] < list_of_integers[mid_index + 1]:
+        return find_peak(list_of_integers[mid_index:])
+    if mid_index > 0 and list_of_integers[mid_index] < list_of_integers[mid_index - 1]:
+        return find_peak(list_of_integers[:mid_index])
+    
