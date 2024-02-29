a57e1e2de13e17a1be2bbbf10b1a69ac8b4566db
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-03-01 00:26:39 +0300

    Avoid pipes

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #  Bash script that sends a request to a URL and displays only the status code of the response
-curl -sw "%{http_code}" "$1" | grep -oE '[0-9]+' |tr -d '\n'
+curl -s -o /dev/null -w "%{http_code}" "$1"
