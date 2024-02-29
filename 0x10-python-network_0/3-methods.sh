ef83bf5bad4075107267bb49b23dd83258bd116e
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-02-29 21:44:38 +0300

    Print all in the same line comma separated

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL and displays all HTTP methods the server will accept.
-curl -sI "$1" | grep -i "Allow" | awk '{print $2}'
+curl -sI 0.0.0.0:5000/route_4 | grep -i "Allow" | awk '{for (i=2; i<=NF; i++) printf "%s%s", $i, (i<NF ? " " : "\n")}'
