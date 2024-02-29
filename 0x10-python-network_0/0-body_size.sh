74fb5109b5bdc410397259d0c828b6c243ed1618
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-02-29 08:05:04 -0800

    Add solution for task 0

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
+# 	The size must be displayed in bytes
+# 	You have to use curl
+URL=$1
+curl -sI "$URL" | grep  'Content-Length:' | cut -d ' ' -f2
