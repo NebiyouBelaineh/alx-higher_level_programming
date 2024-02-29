0e23813244a7f411a2d37d7cffd7de9ba5b6ee60
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-02-29 20:33:58 +0300

    Use location

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-response=$(curl -sw "%{http_code}" "$1" -o response.txt)
-status_code=$(echo "$response" | tail -n 1)
-if [ "$status_code" -eq 200 ]; then
-	cat response.txt
-fi
-rm -f response.txt
+curl -sL "$1"
