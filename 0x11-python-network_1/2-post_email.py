ce9c782dcea92f195030064ca9236469509541e0
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-03-01 21:03:12 +0300

    Move import

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -2,11 +2,11 @@
 """Python script that takes in a URL and an email, sends a POST request to the
 passed URL with the email as a parameter, and displays the body of the response
 (decoded in utf-8)"""
-import urllib.request
-import urllib.parse
-import sys
-
 if __name__ == "__main__":
+    import urllib.request
+    import urllib.parse
+    import sys
+
     url = sys.argv[1]
     email = sys.argv[2]
     var = {'email': email}
