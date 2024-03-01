4c4eb42cff15e84b3eccd782e55cbcf02e1caae4
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-03-01 20:14:51 +0300

    Add solution for task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -14,4 +14,4 @@ if __name__ == "__main__":
     req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as response:
         res = response.read()
-        print("{}".format(res.decode('utf-8')))
+        print(res.decode('utf-8'))
