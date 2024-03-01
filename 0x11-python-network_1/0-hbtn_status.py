975de98f41f10cdc0b4b623f2aa4cdf36f58f72e
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-03-01 18:32:52 +0300

    Add solution for task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# Python script that fetches https://alx-intranet.hbtn.io/status
+import urllib.request
+url = "https://alx-intranet.hbtn.io/status"
+with urllib.request.urlopen(url) as response:
+    res = response.read()
+    print(type(res))
+    print(res)
+    print(res.decode())
