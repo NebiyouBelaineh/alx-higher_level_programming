fcf9cf2789e99423a25df829ebc0d0af0c89689f
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-03-01 18:37:40 +0300

    Fix print formatting

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,7 +3,8 @@
 import urllib.request
 url = "https://alx-intranet.hbtn.io/status"
 with urllib.request.urlopen(url) as response:
+    print("Body response:")
     res = response.read()
-    print(type(res))
-    print(res)
-    print(res.decode('utf-8'))
+    print("\t- type: {}".format(type(res)))
+    print("\t- content: {}".format(res))
+    print("\t- utf8 content: {}".format(res.decode('utf-8')))
