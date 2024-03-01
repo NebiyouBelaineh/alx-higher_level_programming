3cf8778bf32345913d8f53799f9c62eff0b742ba
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-03-01 20:48:55 +0300

    replace variable substitution

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -7,11 +7,11 @@ import urllib.parse
 import sys
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-    email = sys.argv[2]
-    data = urllib.parse.urlencode({'email': email})
+    # url = sys.argv[1]
+    # email = sys.argv[2]
+    data = urllib.parse.urlencode({'email': sys.argv[2]})
     data = data.encode('ascii')
-    req = urllib.request.Request(url, data)
+    req = urllib.request.Request(sys.argv[1], data)
     with urllib.request.urlopen(req) as response:
         res = response.read()
         print(res.decode('utf-8'))
