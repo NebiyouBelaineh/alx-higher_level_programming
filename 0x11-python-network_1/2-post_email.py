ed6e804edbc5f04bb3a01b0c28244cd64dcbbf34
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-03-01 21:07:49 +0300

    Make executable

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,26 +1,17 @@
 #!/usr/bin/python3
-"""fetches https://alx-intranet.hbtn.io/status and
-   get the 'X-Request-Id' value
-"""
-
+"""Python script that takes in a URL and an email, sends a POST request to the
+passed URL with the email as a parameter, and displays the body of the response
+(decoded in utf-8)"""
+import urllib.request
+import urllib.parse
+import sys
 
 if __name__ == "__main__":
-    from urllib import parse, request
-    from urllib.error import HTTPError, URLError
-    import sys
-    import urllib
-
     url = sys.argv[1]
     email = sys.argv[2]
-    data = parse.urlencode({'email': email}).encode('utf-8')
-    req = request.Request(url, data=data)
-    try:
-        with request.urlopen(req, timeout=10) as response:
-            content = response.read().decode('utf-8')
-            print(content)
-    except HTTPError as error:
-        print(error.status, error.reason)
-    except URLError as error:
-        print(error.reason)
-    except TimeoutError:
-        print("Request timed out")
+    data = urllib.parse.urlencode({'email': email})
+    data = data.encode('utf-8')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        res = response.read()
+        print(res.decode('utf-8'))
