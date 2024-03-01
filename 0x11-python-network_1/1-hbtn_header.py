2761ed2e6645742fa3681843411d05ba30474825
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-03-01 19:04:03 +0300

    Use get() to access dictionary

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -5,5 +5,5 @@ import urllib.request
 import sys
 url = sys.argv[1]
 with urllib.request.urlopen(url) as response:
-    header = response.info()
-    print(header['X-Request-Id'])
+    header = response.headers.get('X-Request-Id')
+    print(header)
