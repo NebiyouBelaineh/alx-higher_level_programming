acd9da3ec3f1890fe337c07c53d937f9db930325
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-03-26 21:31:38 +0300

    Add solution for task 2

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// Script that display the status code of a GET request
+
+const request = require('request');
+
+const reqUrl = process.argv[2];
+
+request(reqUrl, function (err, response) {
+  if (err) {
+    console.log(err);
+  }
+  console.log(response && response.statusCode);
+});
