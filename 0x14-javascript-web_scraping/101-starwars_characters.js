ce0c8c3a3d010c686bdd5137b6ad29c6dce8a931
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-03-27 02:20:45 +0300

    Move async function higher

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -5,25 +5,30 @@ const request = require('request');
 const movieId = process.argv[2];
 const reqUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
-request(reqUrl, function (err, response, body) {
+request(reqUrl, async function (err, response, body) {
   if (err) {
-    console.log(err);
+    console.error(err);
     return;
   }
   const responseBody = JSON.parse(body);
   const characters = responseBody.characters;
-  // Make a request for each chatacter to get their name in an asynchronous way
-  characters.forEach(async function (element) {
-    await new Promise(function (resolve, reject) {
-      request(element, function (err, response, body) {
-        if (err) {
-          reject(err);
-        }
-        const resJson = JSON.parse(body);
-        const name = resJson.name;
-        console.log(name);
-        resolve();
+  // Make a request for each character to get their name in an asynchronous way
+  for (const element of characters) {
+    try {
+      const respChar = await new Promise(function (resolve, reject) {
+        request(element, function (err, response, body) {
+          if (err) {
+            reject(err);
+            return;
+          }
+          resolve(body);
+        });
       });
-    });
-  });
+      const resJson = JSON.parse(respChar);
+      const name = resJson.name;
+      console.log(name);
+    } catch (error) {
+      console.error(error);
+    }
+  }
 });
