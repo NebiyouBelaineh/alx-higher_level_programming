3dc841982001936305fc2eb43a217f2ab8f0f0e9
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-04-04 19:58:17 +0300

    Make semistandard compatibility

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -1,12 +1,12 @@
-$(function() {
-    const url = 'https://hellosalut.stefanbohacek.dev/?lang';
-    $("input#btn_translate").on("click", function() {
-        const code = $("input#language_code").val();
-        final_url = url + '=' + code;
-        // console.log(code);
-        $.get(final_url, function(response) {
-            const hello = response.hello;
-            $("div#hello").text(response.hello);
-        });
+$(function () {
+  const url = 'https://hellosalut.stefanbohacek.dev/?lang';
+  $('input#btn_translate').on('click', function () {
+    const code = $('input#language_code').val();
+    const finalUrl = url + '=' + code;
+    // console.log(code);
+    $.get(finalUrl, function (response) {
+      const hello = response.hello;
+      $('div#hello').text(hello);
     });
+  });
 });
--- 0x15-javascript-web_jquery/103-script.js
+++ 0x15-javascript-web_jquery/103-script.js
@@ -1,23 +1,23 @@
-$(function() {
-    const url = 'https://hellosalut.stefanbohacek.dev/?lang';
-    
-    function fetchTranslation() {
-        const code = $("input#language_code").val();
-        const final_url = url + '=' + code;
-        // console.log(code);
-        $.get(final_url, function(response) {
-            const hello = response.hello;
-            $("div#hello").text(response.hello);
-        });
-    }
+$(function () {
+  const url = 'https://hellosalut.stefanbohacek.dev/?lang';
 
-    // Fetch translation when the user clicks the button
-    $("input#btn_translate").on("click", fetchTranslation);
-
-    // Fetch translation when the user presses ENTER in the language_code input field
-    $("input#language_code").keypress(function(event) {
-        if (event.which === 13) { // Check if the ENTER key is pressed
-            fetchTranslation();
-        }
+  function fetchTranslation () {
+    const code = $('input#language_code').val();
+    const finalUrl = url + '=' + code;
+    // console.log(code);
+    $.get(finalUrl, function (response) {
+      const hello = response.hello;
+      $('div#hello').text(hello);
     });
+  }
+
+  // Fetch translation when the user clicks the button
+  $('input#btn_translate').on('click', fetchTranslation);
+
+  // Fetch translation when the user presses ENTER in the language_code input field
+  $('input#language_code').keypress(function (event) {
+    if (event.which === 13) { // Check if the ENTER key is pressed
+      fetchTranslation();
+    }
+  });
 });
